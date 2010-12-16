bb0ce31800014ac226ed027db495ed357f1b1028
Cássio Marques <cassiommc@gmail.com>
2010-12-16 12:33:57 -0200

    Reverting code reorganization for now...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'enumerate_it'
-require 'base'
 require 'spec'
 require 'spec/autorun'
 
