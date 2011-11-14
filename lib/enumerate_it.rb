3dff7b7dfa255ab07b94ce351cafba7438e18543
Cássio Marques <cassiommc@gmail.com>
2011-11-14 15:19:22 -0200

    Refactoring in EnumerateIt::Base.associate_values

--- lib/enumerate_it.rb
+++ lib/enumerate_it.rb
@@ -182,12 +182,7 @@ module EnumerateIt
     @@registered_enumerations = {}
 
     def self.associate_values(*args)
-      if args.first.is_a?(Hash)
-        values_hash = args.first
-      else
-        values_hash = {}
-        args.map{|v| values_hash[v] = v.to_s }
-      end
+      values_hash = args.first.is_a?(Hash) ? args.first : args.inject({}) { |h, v| h[v] = v.to_s; h }
 
       register_enumeration normalize_enumeration(values_hash)
       values_hash.each_pair { |value_name, attributes| define_enumeration_constant value_name, attributes[0] }
