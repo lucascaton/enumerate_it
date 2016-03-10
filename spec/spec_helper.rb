84523731d8abada2a2f60a6fe0ddfe8b49f2a37b
Lucas Caton <lucascaton@gmail.com>
2016-03-10 22:03:35 +1000

    Cleaning up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require "enumerate_it"
 require "rspec"
 require "rspec/autorun"
