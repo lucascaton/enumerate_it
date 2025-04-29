5aff0fa1daefc15d8ea774d409da88b0613aa08e
Lucas Caton <lucascaton@gmail.com>
2025-04-30 08:21:28 +1000

    Use anonymous block arguments forwarding (&)

--- lib/enumerate_it/base.rb
+++ lib/enumerate_it/base.rb
@@ -51,8 +51,8 @@ module EnumerateIt
         list.map { |value| t(value) }
       end
 
-      def each_value(&block)
-        list.each(&block)
+      def each_value(&)
+        list.each(&)
       end
 
       def to_json(options = nil)
